3af6d8d4adbdea2bb03c139bac0f4127784c745f
krishnagoli <34218824+krishnagoli@users.noreply.github.com>
2021-03-12 12:47:28 +0000

    Add files via upload

---
+++ conditionstest.py
@@ -0,0 +1,12 @@
+hrs = input("Enter Hours :")
+rate = input("Enter Rate per hour :")
+
+hrs = float(hrs)
+rate = float(rate)
+
+if hrs<=40:
+   pay = hrs*rate
+   print (pay)
+else:
+   pay = (hrs-40)*(rate*1.5) + 40*rate
+   print (pay)

59c6b9230104ae69d7282b913d5b191befd81def
krishna <krishnak.goli@gmail.com>
2021-03-24 14:20:30 +0000

    test code

---
+++ Harikah/test12.py
@@ -0,0 +1,28 @@
+num = 100
+while (num > 0):
+  if(num%2 != 0):
+      print(num)
+  num = num-1
+  if(num==50):
+      break
+
+
+------
+
+
+num = int(input('enter an integer:'))
+ 
+while (num>1):
+   num = num**2
+   print(num)
+   if(num>100):
+       break
+----
+
+
+char = str(input('enter a single character:'))
+ 
+while (char!=" "):
+   print(char)
+   break
+

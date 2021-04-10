bccbb4cf74bfb4412d640485ae817619b3f04127
krishna <krishnak.goli@gmail.com>
2021-04-10 17:46:52 +0100

    test committ

--- Harikah/workspace.py
+++ Harikah/workspace.py
@@ -1,7 +1,8 @@
-a=[1,2,3,4]
-b=[6,7,8,9]
-a.extend(b)#To append multiple values
-print(a)
+x={'Ganesh':[101,'President',30000,10],
+   'Hari':[102,'Manager',25000,20],
+   'Lakshmi':[103,'Analyst',50000,30],
+   'sai':[104,'Programmer',20000,10]}
 
 
 
+print(x['Ganesh'])#To see the details of Ganesh
